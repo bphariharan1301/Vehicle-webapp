b4f0e74d84810a6f62ad8d458f65a1d0b62edbc6
HARIHARAN B P <bphariharan1301@gmail.com>
2021-11-17 22:32:46 +0530

    .env added

--- vehicle/settings.py
+++ vehicle/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#r+%a=kp3&ebn407f!-!l=-7%^tv$)6jns&1-i+@u#z&kznxhe'
+# SECRET_KEY = 'django-insecure-#r+%a=kp3&ebn407f!-!l=-7%^tv$)6jns&1-i+@u#z&kznxhe'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
